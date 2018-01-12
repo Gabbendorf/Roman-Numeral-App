07ffca8c54b20b072703430a0f372b176e48bbe5
Gabba <gmedas@8thlight.com>
2018-01-12 10:48:01 +0000

    implements basic user interface

---
+++ src/main/java/UI.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class UI {
+
+    private final PrintStream output;
+    private final Scanner input;
+
+    public UI(PrintStream output, InputStream input) {
+        this.output = output;
+        this.input = new Scanner(input);
+    }
+
+    public int askForNumberToConvert() {
+        output.println("Choose a number from 1 to 3999:");
+        return Integer.parseInt(input.nextLine());
+    }
+
+    public void printConvertedNumber(int numberChosen, String convertedNumber) {
+        output.println(String.format("The Roman number for %s is: %s.",
+                Integer.toString(numberChosen), convertedNumber));
+    }
+
+    public String askToPlayAgain() {
+        output.println("Another number to convert? y/n");
+        return input.nextLine();
+    }
+}
---
+++ src/test/java/UITest.java
@@ -0,0 +1,48 @@
+import org.junit.*;
+import java.io.*;
+import static org.junit.Assert.*;
+
+public class UITest {
+
+    private ByteArrayOutputStream output;
+
+    @Before
+    public void setUp() {
+        output = new ByteArrayOutputStream();
+    }
+
+    @Test
+    public void promptsUserForNumberToConvert() {
+        UI ui = newUIWith("3");
+
+        int numberChosen = ui.askForNumberToConvert();
+
+        assertTrue(output.toString().contains("Choose a number from 1 to 3999:"));
+        assertEquals(3, numberChosen);
+    }
+
+    @Test
+    public void printsConvertedRomanNumber() {
+        UI ui = newUIWith("someInput");
+
+        ui.printConvertedNumber(11, "XI");
+
+        assertTrue(output.toString().contains("The Roman number for 11 is: XI."));
+    }
+
+    @Test
+    public void printsMessageToPlayAgain() {
+        UI ui = newUIWith("y");
+
+        String userAnswer = ui.askToPlayAgain();
+
+        assertTrue(output.toString().contains("Another number to convert? y/n"));
+        assertEquals("y", userAnswer);
+    }
+
+    private UI newUIWith(String inputString) {
+        ByteArrayInputStream input = new ByteArrayInputStream(inputString.getBytes());
+
+        return new UI(new PrintStream(output), input);
+    }
+}
